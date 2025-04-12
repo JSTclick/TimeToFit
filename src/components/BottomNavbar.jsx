ecf6a386d735c6b5173e359e571c5bd1798cbff8
ShreyasMP17 <shreyasmp.7@gmail.com>
2025-04-12 12:51:53 +0530

    Fixed  issue

--- src/components/BottomNavbar.jsx
+++ src/components/BottomNavbar.jsx
@@ -1,29 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Home, Dumbbell, Shapes, BookOpen, PlayCircle } from "lucide-react";
 
 const BottomNavBar = () => {
   return (
     <nav className="bg-gray-900 border-t border-gray-700 py-3 px-4 flex justify-around text-xs fixed bottom-0 w-full md:hidden z-50">
-      <div className="flex flex-col items-center text-yellow-400">
+      
+      <Link to="/dashboard" className="flex flex-col items-center text-yellow-400">
         <Home className="w-5 h-5" />
         <span>Home</span>
-      </div>
-      <div className="flex flex-col items-center hover:text-yellow-400 cursor-pointer">
+      </Link>
+
+      <Link to="/workout-page" className="flex flex-col items-center hover:text-yellow-400">
         <Dumbbell className="w-5 h-5" />
         <span>Exercises</span>
-      </div>
-      <div className="flex flex-col items-center hover:text-yellow-400 cursor-pointer">
+      </Link>
+
+      <Link to="/3d-model" className="flex flex-col items-center hover:text-yellow-400">
         <Shapes className="w-5 h-5" />
         <span>3D Model</span>
-      </div>
-      <div className="flex flex-col items-center hover:text-yellow-400 cursor-pointer">
+      </Link>
+
+      <Link to="/theory" className="flex flex-col items-center hover:text-yellow-400">
         <BookOpen className="w-5 h-5" />
         <span>Theory</span>
-      </div>
-      <div className="flex flex-col items-center hover:text-yellow-400 cursor-pointer">
+      </Link>
+
+      <Link to="/training" className="flex flex-col items-center hover:text-yellow-400">
         <PlayCircle className="w-5 h-5" />
         <span>Training</span>
-      </div>
+      </Link>
+
     </nav>
   );
 };
